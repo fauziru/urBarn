bbace45666f0e5cd6ca3bd408c13e6b2d8ea2fad
fauziru <fauzirezaumr@gmail.com>
2021-01-26 19:44:08 +0700

    update mobile layout

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Wrapper>
     <div class="">
-      tes
+      tes halaman about
       <button class="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
         Hover me
       </button>
